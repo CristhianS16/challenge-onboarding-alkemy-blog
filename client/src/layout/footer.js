68a07206a002037700d82ed79fff80ec5f6f6496
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-12 21:04:46 -0300

    Finish the footer

---
+++ client/src/layout/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+      <footer className="col-md-12 bg-default text-white text-center p-3">
+        <p className="m-0">
+          &#169; Challenge from Alkemy by:
+          <a className="footer-link" href="https://github.com/CristhianS16/" target="_blank"><strongo> Cristhian Daniel Strokol</strongo></a>
+        </p>
+      </footer>
+    )
+}
+
+export default Footer;
