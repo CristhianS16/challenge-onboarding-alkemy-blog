822fff143bbb360187214b620bfefe90d0fdd3a6
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-13 20:34:44 -0300

    Change the bg of footer and add norreferrer method for security

--- client/src/layout/footer.js
+++ client/src/layout/footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../assets/css/footer.css';
 
 const Footer = () => {
     return (
-      <footer className="col-md-12 bg-default text-white text-center p-3">
+      <footer className="col-md-12 bg-dark text-white text-center p-3">
         <p className="m-0">
           &#169; Challenge from Alkemy by:
-          <a className="footer-link" href="https://github.com/CristhianS16/" target="_blank"><strongo> Cristhian Daniel Strokol</strongo></a>
+          <a className="footer-link" href="https://github.com/CristhianS16/" target="_blank" rel="noreferrer"><strong> Cristhian Daniel Strokol</strong></a>
         </p>
       </footer>
     )
