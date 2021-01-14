e8c4c3dbef4737a1f0664668c7df56d79d15cda7
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-14 20:12:31 -0300

    Added param findAll for check if user need find all posts or find for pages

--- client/src/helpers/getDataOfPosts.js
+++ client/src/helpers/getDataOfPosts.js
@@ -1,29 +1,34 @@
-import Axios from 'axios';
+import Axios from "axios";
 
-export async function getPosts(since, until) {
-    try {
-        const url = `https://jsonplaceholder.typicode.com/posts?_start=${since}&_end=${until}`;
-        const data = await Axios.get(url);
-        const posts = await data.data;
+export async function getPosts(since, until, findAll) {
+  try {
+    let url;
+    if (findAll) {
+      url = `https://jsonplaceholder.typicode.com/posts`;
+    } else {
+      url = `https://jsonplaceholder.typicode.com/posts?_start=${since}&_end=${until}`;
+    }
+    const data = await Axios.get(url);
+    const posts = await data.data;
 
-        return posts;
-    } catch (error) {
-        if(error.response.status === 404){
-            return error.response.status;
-        };
-    };
-};
+    return posts;
+  } catch (error) {
+    if (error.response.status === 404) {
+      return error.response.status;
+    }
+  }
+}
 
 export async function getPost(id) {
-    try {
-        const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
-        const data = await Axios.get(url);
-        const post = await data.data;
+  try {
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    const data = await Axios.get(url);
+    const post = await data.data;
 
-        return post;
-    } catch (error) {
-        if(error.response.status === 404){
-            return error.response.status;
-        };
-    };
-};
+    return post;
+  } catch (error) {
+    if (error.response.status === 404) {
+      return error.response.status;
+    }
+  }
+}
