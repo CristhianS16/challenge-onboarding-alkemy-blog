10aeaef60e340b1e3e10561ce9e9a0b7b583c8a5
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-13 22:18:59 -0300

    Finish the details of a post and add helpers functions

---
+++ client/src/components/postDetails.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useHistory } from 'react-router-dom';
+
+const PostDetails = ({dataPost: {id, title, body}}) => {
+
+  let history = useHistory();
+
+  return (
+    <div className="row d-flex card">
+      <div className="bg-dark d-flex justify-content-between align-items-center p-2 mb-2">
+        <button 
+          className="btn btn-dark rounded"
+          onClick={ () => {
+              history.goBack()
+          }}
+        ><strong>&lArr; Go back</strong></button>
+        <h2 className="text-white">ID: #{id}</h2>
+      </div>
+      <img
+        src="https://via.placeholder.com/300"
+        alt="placeholder"
+        className="card-img-bottom col"
+      ></img>
+      <div className="col card-body">
+        <h1 className="card-title">{title}</h1>
+        <span className="card-text">{body}</span>
+        <div className="d-flex justify-content-between pt-4">
+          <p className="card-text">
+            Categoría: <strong>Comida</strong>
+          </p>
+          <p className="card-text">
+            <small className="text-muted">16/01/2020</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDetails;
