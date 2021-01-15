0d40f21322652c66db2761bb0a1fd6ee924554d4
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-15 11:59:36 -0300

    Add start and end parameters for paginate and import homeController

--- server/src/api/posts.js
+++ server/src/api/posts.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {getAllPosts} = require('../controllers/homeControllers.js');
 
-router.get('/posts', (req,res) => {
-});
-
+router.get('/posts/:start/:end', getAllPosts);
 module.exports = router;
