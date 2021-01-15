8d852e0ea2f98f5cad0b962d7b1cfffb3660b34f
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-15 15:03:22 -0300

    Added controller for create a new post and your route

--- server/src/api/posts.js
+++ server/src/api/posts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {getAllPosts} = require('../controllers/homeControllers.js');
+const { getAllPosts } = require('../controllers/homeControllers.js');
+const { createPost } = require('../controllers/formCreateController.js');
 
 router.get('/posts/:start/:end', getAllPosts);
+router.post('/posts', createPost);
+
 module.exports = router;
