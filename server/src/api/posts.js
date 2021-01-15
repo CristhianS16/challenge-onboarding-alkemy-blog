ea6c37760961c5eb18be1bdb3ebc046d2f704920
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-15 16:11:32 -0300

    Added controller for delete a post with your route by id

--- server/src/api/posts.js
+++ server/src/api/posts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPosts } = require('../controllers/homeControllers.js');
+const { getAllPosts, deletePost } = require('../controllers/homeControllers.js');
 const { createPost } = require('../controllers/formCreateController.js');
 const updatePost = require('../controllers/formEditController.js');
 
 router.get('/posts/:start/:end', getAllPosts);
 router.post('/posts', createPost);
 router.patch('/posts/:id', updatePost);
+router.delete('/posts/:id', deletePost);
 
 module.exports = router;
--- server/src/controllers/homeControllers.js
+++ server/src/controllers/homeControllers.js
@@ -19,6 +19,26 @@ const getAllPosts = async (req, res) => {
     };
 };
 
+const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if(isNaN(Number(id))){
+        res.status(404).json({message: 'No id specified'});
+    };
+    try {
+        const record = await Post.destroy({
+            where: { id }
+        });
+        if(record === 1){
+            res.status(200).json({message: 'Post deleted'});
+        } else {
+            res.status(404).json({message: 'Post not found'});
+        };
+    } catch (error) {
+        res.status(500).json(error);
+    };
+};
+
 module.exports = {
-    getAllPosts
+    getAllPosts,
+    deletePost
 };
