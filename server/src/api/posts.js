3665e1dfd40e7f505ea5e79fa50df014d29db00f
Cristhian Daniel Strokol <cs.wallpro@gmail.com>
2021-01-15 15:04:52 -0300

    Added controller for update a post and your route

--- server/src/api/posts.js
+++ server/src/api/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllPosts } = require('../controllers/homeControllers.js');
+const { getAllPosts, updatePost } = require('../controllers/homeControllers.js');
 const { createPost } = require('../controllers/formCreateController.js');
 
 router.get('/posts/:start/:end', getAllPosts);
 router.post('/posts', createPost);
+router.patch('/posts/:id', updatePost);
 
 module.exports = router;
--- server/src/controllers/homeControllers.js
+++ server/src/controllers/homeControllers.js
@@ -15,10 +15,42 @@ const getAllPosts = async (req, res) => {
         });
         res.json(posts);
     } catch (error) {
-        console.error(error);
+        console.log('Error: ', error);
     };
 };
 
+const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, content, image, category } = req.body;
+
+    if(!isNaN(Number(id)) && title && content){
+        try {
+            const post = await Post.findOne( {where: { id }} );
+            if(post){
+                const rowsUpdated = await Post.update(
+                    {
+                        title: title,
+                        content: content,
+                        image: image,
+                        category: category
+                    },
+                    {
+                        where: { id }
+                    }
+                );
+                res.json({rows: rowsUpdated});
+            } else {
+                res.json({status: 404, message: 'Post not found'})
+            }
+        } catch (error) {
+            console.log('Error: ', error);
+        };
+    } else {
+        res.json({status: 404, message: 'ID is required for update a post or title and content not especifies'})
+    }
+}
+
 module.exports = {
-    getAllPosts
+    getAllPosts,
+    updatePost
 };
